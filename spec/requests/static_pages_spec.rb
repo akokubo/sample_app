a9661542b3ca773cf880c51631d616e84ae76055
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-08-15 12:18:12 +0900

    full_titleヘルパーを使用するようにstatic_page#homeを変更およびテストの作成

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,10 +14,16 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-    # タイトルに対するテスト
-    it "should have the title 'Home'" do
+    # full_titleヘルパーによりベース・タイトルを表示しているかをテスト
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    # full_titleヘルパーによりタイトルにサフィックスが含まれていないことをテスト
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
