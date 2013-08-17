7e0b33a42ab1aa508a122fb048c37fbae49715dc
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-08-17 15:13:03 +0900

    users#createの成功時のflashメッセージの誤植を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Smaple App!"
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
