cad325b528bc255a9fa98cbe425eef4ec78639e1
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-08-17 21:54:20 +0900

    Userモデルでインスタンスの生成時に記憶トークンを生成、およびテストの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,26 @@
 class User < ActiveRecord::Base
   before_save { email.downcase! }
+  before_create :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   has_secure_password
+
+  # クラスメソッドnew_remember_token
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # クラスメソッドencrypt
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+    def create_remember_token
+      # トークンを作成し、インスタンス変数remember_tokenに保存する
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
