75d556348fbd9c3701255643664b99a8b6986c6a
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-08-19 10:51:25 +0900

    users#indexの追加とテスト

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,9 +30,10 @@ describe "AuthenticationPages" do
       before { sign_in user }
 
       it { should have_title(user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
-      it { should have_link('Settings', href: edit_user_path(user)) }
-      it { should have_link('Sign out', href: signout_path) }
+      it { should have_link('Users',       href: users_path) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
+      it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
       describe "followed by signout" do
@@ -92,6 +93,10 @@ describe "AuthenticationPages" do
         end
       end
 
+      describe "visiting the user index" do
+        before { visit users_path }
+        it { should have_title('Sign in')}
+      end
     end
   end
 end
