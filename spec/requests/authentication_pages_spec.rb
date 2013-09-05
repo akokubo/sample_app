cb5b02bc2038205a3310ad28054ae7711d0008ec
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-09-05 15:41:56 +0900

    Follow/Unfollowボタンの作成とテスト

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -116,6 +116,19 @@ describe "AuthenticationPages" do
         it { should have_title('Sign in')}
       end
 
+      describe "in the Users controller" do
+
+        describe "visitng the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+      end
+
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -126,6 +139,18 @@ describe "AuthenticationPages" do
         describe "submitting to the destroy action" do
           before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submtting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }          
         end
       end
     end
